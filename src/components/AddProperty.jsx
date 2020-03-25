9c7a58a702fc1f5e612361789ae679482f1019fc
Stephen Blair <stephenjohnblair@outlook.com>
2020-03-25 16:53:03 +0000

    Added state and handlers to AddProperty component

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddProperty = () => {
-  return <div>Add Property</div>;
-};
+class AddProperty extends Component {
+  state = {
+    fields: {
+      type: 'Flat',
+      city: 'Manchester',
+      title: '',
+    },
+  };
+
+  handleAddProperty = event => {
+    event.preventDefault();
+    console.log(this.state.fields);
+  };
+
+  handleFieldChange = event => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="AddProperty">
+        <form onSubmit={this.handleAddProperty}>
+          <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
+          <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="EndOfTerrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+          <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Liverpool">Liverpool</option>
+            <option value="Sheffield">Sheffield</option>
+          </select>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddProperty;
